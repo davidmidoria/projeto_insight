b0311107da25099578176f6d2017335e6700da27
Arthur Vilaverde <101434220+G0lg4rthur@users.noreply.github.com>
2023-06-06 18:15:05 -0300

    UpdateBiblioteca
    Métodos de: armazenamento;
                         nomear colunas
                         gerador csv

--- programa/Biblioteca.py
+++ programa/Biblioteca.py
@@ -37,3 +37,33 @@ class criador_csv:# essa classe é responsavel pela obtenção de dados e criaç
                 elif tipo=='a' or tipo== 'w':
                     arquivo.write(informacao+'\n')
         except: self._gerador_csv(informacao,'w')
+        
+    #esse método deve receber uma lista contendo as perguntas e uma lista com as respostas que serão armazenadas
+    #no atributo __valores segundo a resposta do usuario 
+    def armazena_valores_nao_num(self,perguntas,retorno,mensagem_erro):
+            self._valores.extend([self._retorna_resposta(pergunta,retorno,mensagem_erro)for pergunta in perguntas])
+
+    #esse método transforma as respostas em uma string é armazena no atributo __total valores  
+    def salvar_dados(self):
+        if len(self._chaves)==len(self._valores):
+            self._gerador_csv((','.join(self._valores)+','+self._data_e_hora()),'a')
+            self._valores=[]
+
+#   
+    #esse método recebe os nomes de cada coluna da tabela é cria o documento caso ele não.
+    def _nome_colunas(self,chaves):
+        self._gerador_csv((','.join(chaves)+',data,hora'),'r')
+        return chaves
+        
+
+    #esse método cria o csv, além disso ele consegue concatenar todas as entrevistas
+    #se os nomes colunas da tabela escrita forém iguais a das colunas da que será escrita
+
+    def _gerador_csv(self,informacao,tipo):
+        try:
+            with open(self._nome_arquivo+'.csv',tipo,encoding='utf-8') as arquivo:
+                if tipo=='r'and arquivo.readline().strip()!=informacao:
+                    raise
+                elif tipo=='a' or 'w':
+                    arquivo.write(informacao+'\n')
+        except: self._gerador_csv(informacao,'w')
