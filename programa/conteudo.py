8d9370e12a7aebd52befd87341ede9b573cb4309
Alice Calage Campos <calagecamposalice@gmail.com>
2023-06-07 23:43:02 -0300

    comitando o menu da pergunta 4,3 e 2

--- programa/conteudo.py
+++ programa/conteudo.py
@@ -1,3 +1,40 @@
+pergunta4="""
+
+{0}projeto insight
+{1}questionario pergunta 4
+{0}você teria 20 minutos por dia ? 
+{1}===================================================                
+{1}0-{0}não soube responder                            
+{1}1-{0}sim                                             
+{1}2-{0}não{1}                                         
+======================================================{0}
+""".format('\033[94m','\033[97m')+'\ndigite:\033[97m'
+
+pergunta3="""
+
+{0}projeto insight
+{1}questionario pergunta 3
+{0}você acha importante ter um professor lhe 
+auxiliando nesse processo? 
+{1}===================================================                
+{1}0-{0}não soube responder                            
+{1}1-{0}sim                                             
+{1}2-{0}não{1}                                         
+======================================================{0}
+""".format('\033[94m','\033[97m')+'\ndigite:\033[97m'
+
+pergunta2="""
+
+{0}projeto insight
+{1}questionario pergunta 2
+{0}você tem codições de aprender o inglês sozinho?
+{1}===================================================                
+{1}0-{0}não soube responder                            
+{1}1-{0}sim                                             
+{1}2-{0}não{1}                                         
+======================================================{0}
+""".format('\033[94m','\033[97m')+'\ndigite:\033[97m'
+
 genero="""
 
 {0}projeto insight
